8531ff562fa876120b7072ae1f03265d5531ac53
Aviral Srivastava <aviralsrivastava@dhcp-wifi-8021x-155-41-52-38.bu.edu>
2020-01-28 11:06:10 -0500

    wip: adding tests

--- hello.go
+++ hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/kebab-mai-haddi/hello/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
